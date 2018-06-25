76fc93d5d3172e5cc758b676058dcfc447f51a3f
Malikabhc <malika.bouhcine@gmail.com>
2018-06-25 13:49:24 +0200

    correction

--- assets/js/script4.js
+++ assets/js/script4.js
@@ -1,4 +1,6 @@
+// déclaration de la fonction qui permet de vider les champs du formulaire
 function deleteForm(){
+// 
   document.getElementById('lastname').value = '';
   document.getElementById('firstname').value = '';
   document.getElementById('city').value = '';
